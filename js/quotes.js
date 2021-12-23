da49ba2a8755a63d517c64a6a1aaab901848f5e6
minsoo cho <khg04170@gmail.com>
2021-12-23 18:27:03 +0000

    quotes implemented with API

--- js/quotes.js
+++ js/quotes.js
@@ -1,6 +1,7 @@
 const quotesEl = document.querySelector('#quotes');
+const url = "https://type.fit/api/quotes"
 
-fetch("https://type.fit/api/quotes")
+fetch(url)
   .then(response => response.json())
   .then((data) => {
     const randomNum = Math.floor(Math.random() * (data.length - 1) + 1);
