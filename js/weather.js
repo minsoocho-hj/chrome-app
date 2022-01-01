bcb8880f528896c5c4af7039f24da3627fa1e4cb
minsoo cho <khg04170@gmail.com>
2022-01-01 16:03:17 +0000

    weather mettric added

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ function onGeoOK(position) {
   const lat = position.coords.latitude;
   const long = position.coords.longitude;
   console.log(lat, long);
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
   fetch(url).then(response => response.json().then((data) => {
     const weather = document.querySelector("#weather span:first-child")
     const city = document.querySelector("#weather span:last-child")
