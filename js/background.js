158d60331319f86aae71606903b4a6bb6bf4a907
minsoo cho <khg04170@gmail.com>
2022-01-02 14:50:23 +0000

    delete comments

--- js/background.js
+++ js/background.js
@@ -2,13 +2,6 @@ const images = ["1.JPG", "2.JPG", "3.JPG", "4.JPG", "5.JPG"]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-
-//create element
-// const backgroundImg = document.createElement("img");
-// backgroundImg.src = `img/${chosenImage}`
-
 // //add inside of body, html
-// document.body.appendChild(backgroundImg);
 document.body.style.background = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(img/${chosenImage})`;
 document.body.style.backgroundSize = "cover";
-//document.body.style.backgroundImage = "linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) )";
